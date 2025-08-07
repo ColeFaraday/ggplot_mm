30a0206390e522e246297e258655f1158fdd2520
ColeFaraday <colefaraday@gmail.com>
2025-08-07 23:29:08 +0200

    feat: working version of statSmooth (no error band)

--- ggplot/statSmooth.m
+++ ggplot/statSmooth.m
@@ -12,7 +12,6 @@ Options[statSmooth] = {
   "thickness" -> Null,
   "group" -> Null,
   "method" -> "lm",
-  "se" -> False,
   "level" -> 0.95,
   "n" -> 80
 };
@@ -47,7 +46,7 @@ statSmooth[opts : OptionsPattern[]] := Module[{
   ];
   
   (* Compute smoothed lines for each group *)
-  smoothedData = KeyValueMap[Function[{groupKey, groupData},
+  smoothedData = Association[KeyValueMap[Function[{groupKey, groupData},
     Module[{xvals, yvals, xyPairs, sortedPairs, smoothedPoints, fitFunc},
       xvals = extractMappedValues[groupData, OptionValue["x"]];
       yvals = extractMappedValues[groupData, OptionValue["y"]];
@@ -57,39 +56,59 @@ statSmooth[opts : OptionsPattern[]] := Module[{
       xyPairs = Cases[xyPairs, {_?NumericQ, _?NumericQ}];
       
       If[Length[xyPairs] < 2,
-        {}, (* Return empty if insufficient data *)
+        groupKey -> {}, (* Return empty if insufficient data *)
         sortedPairs = SortBy[xyPairs, First];
         
-        (* Create smooth fit based on method *)
-        Switch[OptionValue["method"],
-          "lm",
-          fitFunc = LinearModelFit[sortedPairs, x, x];
-          smoothedPoints = Table[{xval, fitFunc[xval]}, {xval, sortedPairs[[All, 1]]}],
+        (* Create dense sampling of x values for smooth curve *)
+        Module[{xmin, xmax, xDense, fitFunc, predictions, confidenceData},
+          xmin = Min[sortedPairs[[All, 1]]];
+          xmax = Max[sortedPairs[[All, 1]]];
+          xDense = Table[x, {x, xmin, xmax, (xmax - xmin)/(OptionValue["n"] - 1)}];
           
-          "loess", (* Placeholder - could implement LOESS *)
-          fitFunc = LinearModelFit[sortedPairs, x, x];
-          smoothedPoints = Table[{xval, fitFunc[xval]}, {xval, sortedPairs[[All, 1]]}],
+          (* Create smooth fit based on method *)
+          Switch[OptionValue["method"],
+            "lm",
+            fitFunc = LinearModelFit[sortedPairs, x, x, ConfidenceLevel -> OptionValue["level"]];
+            predictions = fitFunc /@ xDense;
+            Module[{predictionIntervals},
+              predictionIntervals = fitFunc["MeanPredictionBands", ConfidenceLevel -> OptionValue["level"]] /. {x ->#} & /@ xDense;
+              confidenceData = Transpose[{xDense, predictions, predictionIntervals[[All, 1]], predictionIntervals[[All, 2]]}]
+            ],
+            
+            "loess", (* Placeholder - simple polynomial for now *)
+            fitFunc = LinearModelFit[sortedPairs, {1, x, x^2}, x, ConfidenceLevel -> OptionValue["level"]];
+            predictions = fitFunc /@ xDense;
+            Module[{predictionIntervals},
+              predictionIntervals = fitFunc["MeanPredictionBands", ConfidenceLevel -> OptionValue["level"]] /@ xDense;
+              confidenceData = Transpose[{xDense, predictions, predictionIntervals[[All, 1]], predictionIntervals[[All, 2]]}]
+            ],
+            
+            _, (* Default to linear *)
+            fitFunc = LinearModelFit[sortedPairs, x, x, ConfidenceLevel -> OptionValue["level"]];
+            predictions = fitFunc /@ xDense;
+            Module[{predictionIntervals},
+              predictionIntervals = fitFunc["MeanPredictionBands", ConfidenceLevel -> OptionValue["level"]] /@ xDense;
+              confidenceData = Transpose[{xDense, predictions, predictionIntervals[[All, 1]], predictionIntervals[[All, 2]]}]
+            ]
+          ];
           
-          _, (* Default to linear *)
-          fitFunc = LinearModelFit[sortedPairs, x, x];
-          smoothedPoints = Table[{xval, fitFunc[xval]}, {xval, sortedPairs[[All, 1]]}]
-        ];
-        
-        (* Create data points for the smoothed line *)
-        MapIndexed[Function[{point, index},
-          Association[
-            OptionValue["x"] -> point[[1]],
-            OptionValue["y"] -> point[[2]],
-            "index" -> index[[1]],
-            (* Preserve aesthetics from the group *)
-            "color_aes" -> Lookup[First[groupData], "color_aes", Black],
-            "alpha_aes" -> Lookup[First[groupData], "alpha_aes", Opacity[1]],
-            "thickness_aes" -> Lookup[First[groupData], "thickness_aes", Automatic]
-          ]
-        ], smoothedPoints]
+          (* Create data points for the smoothed line *)
+          groupKey -> Map[Function[dataPoint,
+            Association[
+              OptionValue["x"] -> dataPoint[[1]], (* x_smooth *)
+              OptionValue["y"] -> dataPoint[[2]], (* y_predicted *)
+              "ymin" -> dataPoint[[3]], (* ymin_ci *)
+              "ymax" -> dataPoint[[4]], (* ymax_ci *) 
+              (* Preserve aesthetics from the group *)
+              "color_aes" -> Lookup[First[groupData], "color_aes", Black],
+              "alpha_aes" -> Lookup[First[groupData], "alpha_aes", Opacity[1]],
+              "thickness_aes" -> Lookup[First[groupData], "thickness_aes", Automatic]
+            ]
+          ], confidenceData]
+        ]
       ]
     ]
-  ], groupedData];
+  ], groupedData]];
   
   (* Return grouped results *)
   smoothedData
